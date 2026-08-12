5aad7e99d8a18832f9ab7ee180c710d9357dda82
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-08-12 23:36:54 +0200

    Add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ATHLETE = "ATHLETE", "Athlete"
+        COACH = "COACH", "Coach"
+        CLUB = "CLUB", "Club"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.ATHLETE,
+    )
+
+    def __str__(self):
+        return self.username
